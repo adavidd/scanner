0df0e5b267d8afdde50c09e3b95c38c3cffddcab
chaim1989 <chaim1989@gmail.com>
2021-02-19 02:03:53 +0200

    Added ScanReciver for ZKC Devices, Added Vibrate when wrong Barcode was scanned, Added blink color animation on Barcode found Green and on not found Red

--- app/src/main/java/co/il/scanner/MainActivity.java
+++ app/src/main/java/co/il/scanner/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -21,6 +27,7 @@ import com.airbnb.lottie.LottieAnimationView;
 
 import java.io.Serializable;
 import java.util.List;
+
 
 import co.il.scanner.login.LoginListAdapter;
 import co.il.scanner.model.LoginUser;
@@ -39,7 +46,8 @@ public class MainActivity extends AppCompatActivity implements OrderAdapter.Orde
     private RecyclerView mRecyclerView;
     private OrderAdapter mOrderAdapter;
     private LoginUser mLoginUser;
-    private final static String SCAN_ACTION = "scan.rcv.message";
+    private final static String SCAN_ACTION_RCV = "com.rcv.message";
+    private final static String SCAN_ACTION_ZKC = "com.zkc.scancode";
     String barcodeStr = "";
     private LottieAnimationView mScanning;
     boolean isScanning = false;
@@ -203,41 +211,77 @@ public class MainActivity extends AppCompatActivity implements OrderAdapter.Orde
                 byte temp = intent.getByteExtra("barcodeType", (byte) 0);
                 android.util.Log.i("debug", "----codetype--" + temp);
                 barcodeStr = new String(barocode, 0, barocodelen);
-
                 mScanning.cancelAnimation();
                 checkIfBarcodeExist(barcodeStr);
 
                 Toast.makeText(context, barcodeStr, Toast.LENGTH_SHORT).show();
             }
         };
+        BroadcastReceiver broadcastReceiver1 = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
 
+
+                    barcodeStr = intent.getExtras().getString("code");
+
+                mScanning.cancelAnimation();
+
+                checkIfBarcodeExist(barcodeStr);
+
+                Toast.makeText(context, barcodeStr, Toast.LENGTH_SHORT).show();
+            }
+        };
 
         IntentFilter filter = new IntentFilter();
-        filter.addAction(SCAN_ACTION);
+        filter.addAction(SCAN_ACTION_RCV);
         registerReceiver(broadcastReceiver, filter);
 
+        IntentFilter filter1 = new IntentFilter();
+        filter1.addAction(SCAN_ACTION_ZKC);
+        registerReceiver(broadcastReceiver1, filter1);
 
     }
 
 
-    private void checkIfBarcodeExist(String barcodeStr) {
 
+    private void checkIfBarcodeExist(String barcodeStr) {
+        boolean itemMatched = false;
         if (mOrdersList != null){
 
             for (int i = 0; i < mOrdersList.size(); i++) {
 
-                if (mOrdersList.get(i).getItem().getBarcode1().equals(barcodeStr)) {
-
+                if (mOrdersList.get(i).getItem().getBarcode1().equals(barcodeStr) || mOrdersList.get(i).getItem().getBarcode2().equals(barcodeStr)) {
+                    itemMatched = true;
                     mOrdersList.get(i).setCollectedQuantity(mOrdersList.get(i).getCollectedQuantity() + 1);
                     mOrderAdapter.notifyDataSetChanged();
+                    blink(Color.GREEN);
                     break;
                 }
 
+
+            }
+            if(!itemMatched){
+                Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(500);
+                blink(Color.RED);
             }
         }
 
 
-    }
 
+    }
+    private void blink(int c){
+        final LinearLayout layout = (LinearLayout) findViewById(R.id.MA_recycler_linear_LL);
+        final AnimationDrawable drawable = new AnimationDrawable();
+        final Handler handler = new Handler();
+
+
+        drawable.addFrame(new ColorDrawable(c), 400);
+        drawable.addFrame(new ColorDrawable(Color.WHITE), 400);
+        drawable.setOneShot(true);
+
+        layout.setBackgroundDrawable(drawable);
+        handler.postDelayed(() -> drawable.start(), 100);
+    }
 
 }
